5a20408a542764724aeec0b66ec5c66902947cff
RandleH <40454051+RandleH@users.noreply.github.com>
2021-02-22 23:24:41 +0800

    Add files via upload

--- XCode_12.4/XCode_12.4/main.c
+++ XCode_12.4/XCode_12.4/main.c
@@ -5,21 +5,32 @@
 //  Created by Randle.Helmslay on 2021/2/18.
 //
 
-#include "RH_image.h"
-#include <stdio.h>
 
+#include <stdio.h>
+#include "RH_data.h"
+#include "RH_lib.h"
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    const char* __restrict__ src  = "/Users/randle_h/desktop/lenna.bmp";
-    const char* __restrict__ des  = "/Users/randle_h/desktop/lenna_.bmp";
-
-    __ImageRGB888_t* IMG_IN  = __LoadBMP_ImgRGB888(src);
-
-    __ImageRGB888_t* IMG_OUT = __Create_ImgRGB888(IMG_IN->width, IMG_IN->height);
-    __Blur_Average_ImgRGB888(IMG_IN,IMG_OUT,80000,100);
-    __Blur_Average_ImgRGB888(IMG_OUT,IMG_IN,80000,100);
-    __OutBMP_ImgRGB888(des, IMG_IN);
-    printf("Hello, World!\n");
+    __LinkLoopNode *PH,*P1,*P2,*P3,*P4,*P5,*P6;
+    __LINK_Loop_createHeadNode(&PH);
+    __LINK_Loop_createNode(&P1);
+    __LINK_Loop_createNode(&P2);
+    __LINK_Loop_createNode(&P3);
+    __LINK_Loop_createNode(&P4);
+    __LINK_Loop_createNode(&P5);
+    __LINK_Loop_createNode(&P6);
+    __LINK_Loop_addNode_front(&PH, &P1);
+    __LINK_Loop_addNode_front(&PH, &P2);
+    __LINK_Loop_addNode_front(&PH, &P3);
+    __LINK_Loop_addNode_front(&PH, &P4);
+    __LINK_Loop_addNode_front(&PH, &P5);
+    __LINK_Loop_addNode_front(&PH, &P6);
+    __LINK_Loop_printAllNodesAdr(&PH, printf);
+    
+    __btoa_BIN(0x12);
+    float i =10.9;
+    printf("%s\n",__ftoa_BIN(i));    
+    
     return 0;
 }
